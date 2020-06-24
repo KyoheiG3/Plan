55effdb57c56a1332e6512c895a0f11d6f1cdf3e
Kyohei Ito <je.suis.kyohei+github@gmail.com>
2020-06-24 18:43:47 +0900

    Fix Interactor

--- Plan/Interactor.swift
+++ Plan/Interactor.swift
@@ -1,4 +1,5 @@
 open class Interactor<Action> {
+    public typealias Action = Action
     public let dispatcher: AnyDispatcher<Action>
 
     public init<D: Dispatchable>(dispatcher: D) where D.Action == Action {
